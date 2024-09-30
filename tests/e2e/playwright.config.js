f84133e251b9d2cb346bc57fca95d9ea4a707af1
hh4jv7@gmail.com <hh4jv7@gmail.com>
2024-09-30 14:17:30 +0900

    playwrightのデバッグ修正

--- tests/e2e/playwright.config.js
+++ tests/e2e/playwright.config.js
@@ -31,14 +31,14 @@ export default defineConfig({
       name: "Chromium",
       use: { browserName: "chromium" },
     },
-    {
-      name: "Firefox",
-      use: { browserName: "firefox" },
-    },
-    {
-      name: "WebKit",
-      use: { browserName: "webkit" },
-    },
+    // {
+    //   name: "Firefox",
+    //   use: { browserName: "firefox" },
+    // },
+    // {
+    //   name: "WebKit",
+    //   use: { browserName: "webkit" },
+    // },
   ],
 
   // レポートの設定
